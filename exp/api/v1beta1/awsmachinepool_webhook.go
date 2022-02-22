9014d8221045cf3756062be60a0e872d8f68cff0
shivi28 <sishivani@vmware.com>
2022-02-22 16:05:23 +0530

    Set AZ as nil if passed empty

--- exp/api/v1beta1/awsmachinepool_webhook.go
+++ exp/api/v1beta1/awsmachinepool_webhook.go
@@ -131,4 +131,8 @@ func (r *AWSMachinePool) Default() {
 		log.Info("DefaultCoolDown is zero, setting 300 seconds as default")
 		r.Spec.DefaultCoolDown.Duration = 300 * time.Second
 	}
+
+	if len(r.Spec.AvailabilityZones) == 1 && r.Spec.AvailabilityZones[0] == "" {
+		r.Spec.AvailabilityZones = nil
+	}
 }
